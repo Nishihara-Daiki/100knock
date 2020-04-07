1775a264451321ceb69797ec3464824479517aba
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2020-04-07 23:28:12 +0900

    q40

---
+++ chap05/q40.py
@@ -0,0 +1,46 @@
+"""
+	40. 係り受け解析結果の読み込み（形態素）Permalink
+	形態素を表すクラスMorphを実装せよ．
+	このクラスは表層形（surface），基本形（base），品詞（pos），
+	品詞細分類1（pos1）をメンバ変数に持つこととする．
+	さらに，CaboChaの解析結果（neko.txt.cabocha）を読み込み，
+	各文をMorphオブジェクトのリストとして表現し，3文目の形態素列を表示せよ．
+"""
+
+import xml.etree.ElementTree as ET
+import sys
+
+class Morph():
+	def __init__(self, surface='', base='', pos='', pos1=''):
+		self.surface = surface
+		self.base = base
+		self.pos = pos
+		self.pos1 = pos1
+
+	def __str__(self):
+		return "\t".join([self.surface, self.base, self.pos, self.pos1])
+
+
+
+tree = ET.parse(sys.argv[1])
+document = tree.getroot()
+
+sentences = list()
+
+for sentence in document:
+	morphs = list()
+	for chunk in sentence:
+		for morph in chunk:
+			m = morph.attrib['feature'].split(',')
+			surface = morph.text
+			pos = m[0]
+			pos1 = m[1]
+			base = m[6]
+			morph = Morph(surface, pos, pos1, base)
+			morphs.append(morph)
+	sentences.append(morphs)
+
+
+for morph in sentences[2]:
+	print(morph)
+
--- chap05/run.sh
+++ chap05/run.sh
@@ -1,1 +1,48 @@
 #!/bin/bash
+
+mkdir -p data
+
+wget -P data https://nlp100.github.io/data/neko.txt
+cat <(echo '<root>') \
+	<(cat data/neko.txt | cabocha -I0 -O4 -f3) \
+	<(echo '</root>') \
+	> data/neko.txt.cabocha
+
+
+python3 q40.py data/neko.txt.cabocha
+# 　	記号	空白	　
+# 吾輩	名詞	代名詞	吾輩
+# は	助詞	係助詞	は
+# 猫	名詞	一般	猫
+# で	助動詞	*	だ
+# ある	助動詞	*	ある
+# 。	記号	句点	。
+
+
+python3 q41.py data/neko.txt.cabocha
+
+
+python3 q42.py data/neko.txt.cabocha
+
+
+python3 q43.py data/neko.txt.cabocha
+
+
+python3 q44.py data/neko.txt.cabocha
+
+
+python3 q45.py data/neko.txt.cabocha
+
+
+python3 q46.py data/neko.txt.cabocha
+
+
+python3 q47.py data/neko.txt.cabocha
+
+
+python3 q48.py data/neko.txt.cabocha
+
+
+python3 q49.py data/neko.txt.cabocha
+
+
