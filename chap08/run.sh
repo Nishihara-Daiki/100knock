6a8711666a75d2434b9ee2e0c8a0cdf828cc4d3f
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2020-04-20 22:37:54 +0900

    templete

--- chap08/run.sh
+++ chap08/run.sh
@@ -1,1 +1,36 @@
 #!/bin/bash
+
+mkdir -p data
+# cd data
+# ln -s ../../chap06/data/train.txt
+# ln -s ../../chap06/data/valid.txt
+# ln -s ../../chap06/data/test.txt
+# cd ..
+
+python3 q71.py
+
+
+python3 q72.py
+
+
+python3 q73.py
+
+
+python3 q74.py
+
+
+python3 q75.py
+
+
+python3 q76.py
+
+
+python3 q77.py
+
+
+python3 q78.py
+
+
+python3 q79.py
+
+
